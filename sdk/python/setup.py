031128df0efe9720806df3731447be823519b608
mario-guerra <mario.guerra@gmail.com>
2026-07-23 20:43:16 +0000

    Sync from private repo @ f87092d49c61

--- sdk/python/setup.py
+++ sdk/python/setup.py
@@ -11,7 +11,7 @@ if os.path.exists(readme_path):
 
 setup(
     name="orchid-sdk",
-    version="0.3.3",
+    version="0.3.4",
     packages=find_packages(exclude=["tests", "tests.*"]),
     install_requires=[
         "httpx>=0.20.0",
